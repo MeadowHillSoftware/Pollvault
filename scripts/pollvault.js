b34ee2eda6f095d0a624b56779c76bbd3817a8c9
MeadowHillSoftware <meadowhillsoftware@aol.com>
2018-07-20 12:58:10 -0700

    votes

--- scripts/pollvault.js
+++ scripts/pollvault.js
@@ -1,12 +1,43 @@
 //Pollvault
 //
 //AGPL 3 or later
+//Match Text (Optional) = text entry
+//Date Submitted (Optional) From: = pulldown month, pulldown date, text entry year, calendar button
+//Date Submitted (Optional) To: = pulldown month, pulldown date, text entry year, calendar button
+//Date Updated (Optional) From: = pulldown month, pulldown date, text entry year
+//Date Updated (Optional) To: = pulldown month, pulldown date, text entry year
+//Max Number Results: pulldown (10, 50, 100, 200, 400, Show All)
+//Category: pulldown (Doesn't Matter, BioWare Contest, Capture The Flag, City Adventure, Classic PnP Conversion, Combat, D20 Modern, Deathmatch Arenas, Dramatic, Dungeon Adventure, Epic, Final Version, Forest Adventure, Hardcore Rules, Horror/Survival Horror, Humorous/Parody, Kid Friendly, Mountain Adventure, None/Freeform, Official Campaign, Old BioWare Contest, Other, Persistent Worlds, Puzzles, PvP, Roleplay, Romance, Snowy Adventure, Temple/Crypt Adventure, Tutorial, Twisted/Dark, Uses Stone of Recall/Town Portal, Various Settings)
+//Don't Show This Category: pulldown (same as above except replace Doesn't Matter with Show All)
+//Minimum Rating (Needs 10 votes): pulldown (Doesn't Matter, 10.00, 9.75, 9.50, 9.25, 9.00, 8.75, 8.50, 8.25, 8.00, 7.75, 7.50, 7.25, 7.00, 6.75, 6.50, 6.25, 6.00, 5.75, 5.50, 5.25, 5.00, 4.75, 4.50, 4.25, 4.00, 3.75, 3.50, 3.25, 3.00, 2.75, 2.50, 2.25, 2.00, 1.75, 1.50, 1.25, 1.00)
+//Hall of Fame: pulldown menu (Doesn't Matter, Yes, No)
+//Which Version?: pulldown (Doesn't Matter, All NWN/No SoU/No HotU/No CEP, All SoU, All HotU, All CEP, Works on all versions, CEP-1.00 Requires Community Expansion Project, CEP-1.01 Requires Community Expansion Project, CEP-1.50 Requires Community Expansion Project, CEP-1.51 Requires Community Expansion Project, CEP-1.52 Requires Community Expansion Project, CEP-2.00 Requires Community Expansion Project, Requires SoU & CEP, Requires HotU & CEP, Requires Both Expansions (SoU & HotU), Requires All Expasions (SoU & HotU & CEP), HotU-1.69, HotU-1.68, HotU-1.67, HotU-1.66, HotU-1.65, HotU-1.64, HotU-1.63, HotU-1.62, HotU-1.61, HotU-1.59, SoU-1.69, SoU-1.68, SoU-1.67, SoU-1.66, SoU-1.65, SoU-1.64, SoU-1.63, SoU-1.62, SoU-1.61, SoU-1.32, SoU-1.31, SoU-1.30, NWN-1.69, NWN-1.68, NWN-1.67, NWN-1.66, NWN-1.65, NWN-1.64, NWN-1.63, NWN-1.62, NWN-1.61, NWN-1.32, NWN-1.31, NWN-1.30, NWN-1.29 or lower, NWN-1.30, NWN-1.29, NWN-1.28, NWN-1.27, NWN-1.26, NWN-1.25, NWN-1.24, NWN-1.23, NWN-1.22, NWN-1.21, NWN-1.20, NWN-1.19, NWN-1.18)
+//Level: From: pulldown menu (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40), To: pulldown menu (same as from)
+//Level: Include those listed as "Any" pulldown menu (Yes, No)
+//Number Players: From: pulldown menu (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64), To: pulldown menu (same as from)
+//Number Players: Include those listed as "Any" pulldown menu (Yes, No)
+//Gameplay Hours: From: pulldown menu (Doesn't Matter, <1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 60+), To: pulldown menu (same as from)
+//Single or Multiplayer: pulldown menu (Doesn't Matter, Single Player, Multiplayer, Single Player or Multiplayer)
+//DM Needed?: pulldown menu (Doesn't Matter, No DM Required, Occasional DM Needed, DM Required, Multiple DMs Required, Part of Series)
+//Alignments: text entry
+//Races: text entry
+//Classes: text entry
+//Setting: text entry
+//Tricks & Traps: pulldown menu (Doesn't Matter, Non-Existent, Dependent on Module, Light, Medium, Heavy)
+//Roleplay: pulldown menu (Doesn't Matter, Non-Existent, Dependent on Module, Light, Medium, Heavy)
+//Hack & Slash: pulldown menu (Doesn't Matter, Non-Existent, Dependent on Module, Light, Medium, Heavy)
+//Scope: pulldown menu (Doesn't Matter, Small, Dependent on Module, Medium, Large, Epic, Part of Series)
+//Content Rating: pulldown menu (Doesn't Matter, Everyone, Teen, Mature, Adult, Extreme)
+//Language: pulldown menu (Doesn't Matter, Asian, English, Chinese, French, German, Italian, Japanese, Korean, Russian, Spanish, Other)
+//Show Screenshots: pulldown menu (No, Yes)
+//Submit: button SEARCH
 
 "use strict";
 
 var oPollvault = {};
 
 oPollvault.addMainEventListeners = function() {
+    $('#search-button').on('click', oPollvault.handleSearchButtonClick);
     $('#type').on('change', oPollvault.handleFileUpload);
 };
 
@@ -27,6 +58,50 @@ oPollvault.displayResults = function(oObject, sType) {
             .append(link)
             .append('<br>');
     }
+};
+
+oPollvault.greaterThanOrEqualTo = function(oObject, sField, iValue) {
+    var aMods = Object.keys(oObject);
+    var oResults = {};
+    for (var m = 0; m < aMods.length; m++) {
+        var sFolder = aMods[m]
+        var oMod = oObject[sFolder]
+        var aFields = Object.keys(oMod);
+        if (aFields.indexOf(sField) !== -1) {
+            var iInteger = oMod[sField];
+            if (iInteger >= iValue) {
+                oResults[sFolder] = oMod;
+            }
+        }
+    }
+    return oResults;
+};
+
+oPollvault.handleSearchButtonClick = function(event) {
+    event.stopPropagation();
+    var sVotes = $('#votes').val();
+    if (sVotes !== "Doesn't Matter") {
+        var iVotes = Number(sVotes);
+        var oResults = oPollvault.greaterThanOrEqualTo(oPollvault.oCurrentType, "Votes", iVotes);
+    }
+    oPollvault.displayResults(oResults, "modules");
+};
+
+oPollvault.lessThanOrEqualTo = function(oObject, sField, iValue) {
+    var aMods = Object.keys(oObject);
+    var oResults = {};
+    for (var m = 0; m < aMods.length; m++) {
+        var sFolder = aMods[m]
+        var oMod = oObject[sFolder]
+        var aFields = Object.keys(oMod);
+        if (aFields.indexOf(sField) !== -1) {
+            var iInteger = oMod[sField];
+            if (iInteger <= iValue) {
+                oResults[sFolder] = oMod;
+            }
+        }
+    }
+    return oResults;
 };
 
 oPollvault.handleFileUpload = function(event) {
@@ -51,13 +126,13 @@ oPollvault.handleType = function(event) {
     event.stopPropagation();
     var sType = oPollvault.reader.result;
     oPollvault.oCurrentType = JSON.parse(sType);
-    var oResults = oPollvault.greaterThanOrEqualTo(oPollvault.oCurrentType, "Votes", 10);
-    oResults = oPollvault.greaterThanOrEqualTo(oResults, "Rating", 7.0);
-    oResults = oPollvault.matchOneString(oResults, "Single or Multiplayer", ["Multiplayer", "Single Player or Multiplayer"]);
-    oResults = oPollvault.searchByString(oResults, "DM Needed", "No DM Required");
-    oResults = oPollvault.MinMaxCharacterLevel(oResults, "Max Character Level", 12);
-    oResults = oPollvault.MinMaxCharacterLevel(oResults, "Min Character Level", 10);
-    oPollvault.displayResults(oResults, "modules");
+    //var oResults = oPollvault.greaterThanOrEqualTo(oPollvault.oCurrentType, "Votes", 10);
+    //oResults = oPollvault.greaterThanOrEqualTo(oResults, "Rating", 7.0);
+    //oResults = oPollvault.matchOneString(oResults, "Single or Multiplayer", ["Multiplayer", "Single Player or Multiplayer"]);
+    //oResults = oPollvault.searchByString(oResults, "DM Needed", "No DM Required");
+    //oResults = oPollvault.MinMaxCharacterLevel(oResults, "Max Character Level", 12);
+    //oResults = oPollvault.MinMaxCharacterLevel(oResults, "Min Character Level", 10);
+    //oPollvault.displayResults(oResults, "modules");
 };
 
 oPollvault.matchOneString = function(oObject, sField, aValues) {
@@ -70,40 +145,6 @@ oPollvault.matchOneString = function(oObject, sField, aValues) {
         if (aFields.indexOf(sField) !== -1) {
             var sString = oMod[sField];
             if (aValues.indexOf(sString) !== -1) {
-                oResults[sFolder] = oMod;
-            }
-        }
-    }
-    return oResults;
-};
-
-oPollvault.greaterThanOrEqualTo = function(oObject, sField, iValue) {
-    var aMods = Object.keys(oObject);
-    var oResults = {};
-    for (var m = 0; m < aMods.length; m++) {
-        var sFolder = aMods[m]
-        var oMod = oObject[sFolder]
-        var aFields = Object.keys(oMod);
-        if (aFields.indexOf(sField) !== -1) {
-            var iInteger = oMod[sField];
-            if (iInteger >= iValue) {
-                oResults[sFolder] = oMod;
-            }
-        }
-    }
-    return oResults;
-};
-
-oPollvault.lessThanOrEqualTo = function(oObject, sField, iValue) {
-    var aMods = Object.keys(oObject);
-    var oResults = {};
-    for (var m = 0; m < aMods.length; m++) {
-        var sFolder = aMods[m]
-        var oMod = oObject[sFolder]
-        var aFields = Object.keys(oMod);
-        if (aFields.indexOf(sField) !== -1) {
-            var iInteger = oMod[sField];
-            if (iInteger <= iValue) {
                 oResults[sFolder] = oMod;
             }
         }
