d69350063af1991907b01334ab6e3828f9a949a5
MeadowHillSoftware <meadowhillsoftware@aol.com>
2018-07-25 13:17:29 -0700

    Number of Players

--- scripts/pollvault.js
+++ scripts/pollvault.js
@@ -1,24 +1,11 @@
 //Pollvault
 //
 //AGPL 3 or later
-//Match Text (Optional) = text entry
-//Date Submitted (Optional) From: = dropdown month, dropdown date, text entry year, calendar button
-//Date Submitted (Optional) To: = dropdown month, dropdown date, text entry year, calendar button
-//Date Updated (Optional) From: = dropdown month, dropdown date, text entry year
-//Date Updated (Optional) To: = dropdown month, dropdown date, text entry year
 //Max Number Results: dropdown (10, 50, 100, 200, 400, Show All)
-//Category: dropdown (Doesn't Matter, BioWare Contest, Capture The Flag, City Adventure, Classic PnP Conversion, Combat, D20 Modern, Deathmatch Arenas, Dramatic, Dungeon Adventure, Epic, Final Version, Forest Adventure, Hardcore Rules, Horror/Survival Horror, Humorous/Parody, Kid Friendly, Mountain Adventure, None/Freeform, Official Campaign, Old BioWare Contest, Other, Persistent Worlds, Puzzles, PvP, Roleplay, Romance, Snowy Adventure, Temple/Crypt Adventure, Tutorial, Twisted/Dark, Uses Stone of Recall/Town Portal, Various Settings)
-//Don't Show This Category: dropdown (same as above except replace Doesn't Matter with Show All)
-//Minimum Rating (Needs 10 votes): dropdown (Doesn't Matter, 10.00, 9.75, 9.50, 9.25, 9.00, 8.75, 8.50, 8.25, 8.00, 7.75, 7.50, 7.25, 7.00, 6.75, 6.50, 6.25, 6.00, 5.75, 5.50, 5.25, 5.00, 4.75, 4.50, 4.25, 4.00, 3.75, 3.50, 3.25, 3.00, 2.75, 2.50, 2.25, 2.00, 1.75, 1.50, 1.25, 1.00)
 //Hall of Fame: dropdown menu (Doesn't Matter, Yes, No)
-//Which Version?: dropdown (Doesn't Matter, All NWN/No SoU/No HotU/No CEP, All SoU, All HotU, All CEP, Works on all versions, CEP-1.00 Requires Community Expansion Project, CEP-1.01 Requires Community Expansion Project, CEP-1.50 Requires Community Expansion Project, CEP-1.51 Requires Community Expansion Project, CEP-1.52 Requires Community Expansion Project, CEP-2.00 Requires Community Expansion Project, Requires SoU & CEP, Requires HotU & CEP, Requires Both Expansions (SoU & HotU), Requires All Expasions (SoU & HotU & CEP), HotU-1.69, HotU-1.68, HotU-1.67, HotU-1.66, HotU-1.65, HotU-1.64, HotU-1.63, HotU-1.62, HotU-1.61, HotU-1.59, SoU-1.69, SoU-1.68, SoU-1.67, SoU-1.66, SoU-1.65, SoU-1.64, SoU-1.63, SoU-1.62, SoU-1.61, SoU-1.32, SoU-1.31, SoU-1.30, NWN-1.69, NWN-1.68, NWN-1.67, NWN-1.66, NWN-1.65, NWN-1.64, NWN-1.63, NWN-1.62, NWN-1.61, NWN-1.32, NWN-1.31, NWN-1.30, NWN-1.29 or lower, NWN-1.30, NWN-1.29, NWN-1.28, NWN-1.27, NWN-1.26, NWN-1.25, NWN-1.24, NWN-1.23, NWN-1.22, NWN-1.21, NWN-1.20, NWN-1.19, NWN-1.18)
-//Level: From: dropdown menu (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40), To: dropdown menu (same as from)
-//Level: Include those listed as "Any" dropdown menu (Yes, No)
 //Number Players: From: dropdown menu (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64), To: dropdown menu (same as from)
 //Number Players: Include those listed as "Any" dropdown menu (Yes, No)
 //Gameplay Hours: From: dropdown menu (Doesn't Matter, <1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 60+), To: dropdown menu (same as from)
-//Single or Multiplayer: dropdown menu (Doesn't Matter, Single Player, Multiplayer, Single Player or Multiplayer)
-//DM Needed?: dropdown menu (Doesn't Matter, No DM Required, Occasional DM Needed, DM Required, Multiple DMs Required, Part of Series)
 //Alignments: text entry
 //Races: text entry
 //Classes: text entry
@@ -239,7 +226,7 @@ oPollvault.handleSearchButtonClick = function(event) {
         oResults = oPollvault.searchByString(oResults, "Category", sCategory);
     }
     var sCategory = $('#exclude-category').val();
-    if (sCategory !== "Doesn't Matter") {
+    if (sCategory !== "Show All") {
         oResults = oPollvault.excludeByString(oResults, "Category", sCategory);
     }
     var sVotes = $('#votes').val();
@@ -263,6 +250,13 @@ oPollvault.handleSearchButtonClick = function(event) {
     var sMaxLevel = $('#max-level').val();
     var iMaxLevel = Number(sMaxLevel);
     oResults = oPollvault.minMaxCharacterLevel(oResults, "Max Character Level", iMaxLevel, sAnyLevel);
+    var sAnyPlayers = $('#any-players').val();
+    var sMinPlayers = $('#min-players').val();
+    var iMinPlayers = Number(sMinPlayers);
+    oResults = oPollvault.minMaxPlayerNumbers(oResults, "Min # Players", iMinPlayers, sAnyPlayers);
+    var sMaxPlayers = $('#max-players').val();
+    var iMaxPlayers = Number(sMaxPlayers);
+    oResults = oPollvault.minMaxPlayerNumbers(oResults, "Max # Players", iMaxPlayers, sAnyPlayers);
     var sMultiplayer = $('#multiplayer').val();
     if (sMultiplayer !== "Doesn't Matter") {
         var aMultiplayer = [];
@@ -381,6 +375,32 @@ oPollvault.minMaxCharacterLevel = function(oObject, sField, iValue, sAny) {
                 oResults[sFolder] = oMod;
             }
             if (sField === "Max Character Level") {
+                if (iInteger >= iValue) {
+                    oResults[sFolder] = oMod;
+                }
+            } else {
+                if (iInteger <= iValue) {
+                    oResults[sFolder] = oMod;
+                }
+            }
+        }
+    }
+    return oResults;
+};
+
+oPollvault.minMaxPlayerNumbers = function(oObject, sField, iValue, sAny) {
+    var aMods = Object.keys(oObject);
+    var oResults = {};
+    for (var m = 0; m < aMods.length; m++) {
+        var sFolder = aMods[m];
+        var oMod = oObject[sFolder];
+        var aFields = Object.keys(oMod);
+        if (aFields.indexOf(sField) !== -1) {
+            var iInteger = oMod[sField];
+            if (sAny === "Yes" && iInteger === "Any") {
+                oResults[sFolder] = oMod;
+            }
+            if (sField === "Max # Players") {
                 if (iInteger >= iValue) {
                     oResults[sFolder] = oMod;
                 }
