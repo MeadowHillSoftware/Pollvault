4e294efe95cd5072a7cd00cb094be15e39a581d6
MeadowHillSoftware <meadowhillsoftware@aol.com>
2018-10-24 18:03:47 -0700

    nwn2 scripts awards

--- scripts/pollvault.js
+++ scripts/pollvault.js
@@ -937,7 +937,7 @@ oPollvault.handleSearchButtonClick = function(event) {
         var iRating = Number(sRating);
         oResults = oPollvault.greaterThanOrEqualTo(oResults, "Rating", iRating);
     }
-    if (sType === "creatures" || sType === "hakpaks" || sType === "models" || sType === "nwn2hakpakscombined" || sType === "nwn2hakpaksoriginal" || sType === "nwn2models" || sType === "nwn2other" || sType === "nwn2plugins" || sType === "nwn2prefabareas" || sType === "other" || sType === "portraits" || sType === "prefabs" || sType === "scripts" || sType === "sounds" || sType === "textures") {
+    if (sType === "creatures" || sType === "hakpaks" || sType === "models" || sType === "nwn2hakpakscombined" || sType === "nwn2hakpaksoriginal" || sType === "nwn2models" || sType === "nwn2other" || sType === "nwn2plugins" || sType === "nwn2prefabareas" || sType === "nwn2scripts" || sType === "other" || sType === "portraits" || sType === "prefabs" || sType === "scripts" || sType === "sounds" || sType === "textures") {
         var sAward = $('#hakpaks-awards').val();
         if (sAward !== "Doesn't Matter") {
             oResults = oPollvault.searchForAwards(oResults, sAward);
@@ -1585,7 +1585,8 @@ oPollvault.handleType = function(event) {
     } else if (sName === "nwn2scripts") {
         var aIds = ['#nwn2scripts-category-row', 
             '#nwn2scripts-exclude-category-row', '#votes-row', 
-            '#rating-row', '#nwn2-patch-row', '#button-row'];
+            '#rating-row', '#hakpaks-awards-row', '#nwn2-patch-row', 
+            '#button-row'];
         oPollvault.populateSearchTable(aIds);
         title = $('<b>Search NWN2 Scripts</b>');
     } else if (sName === "nwn2strategies") {
